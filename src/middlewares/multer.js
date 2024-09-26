3e261bf188667c892201a97cff499d1a7ddec6f8
dushimec <dushimec515@gmail.com>
2024-09-26 11:33:10 +0200

    i have added the vercel.json file configuration

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -3,4 +3,4 @@ import multer from "multer";
 // Use memory storage for Multer
 const storage = multer.memoryStorage();
 
-export const singleUpload = multer({ storage }).single("file"); // Field name must match the frontend form data
+export const singleUpload = multer({ storage }).single("file");
